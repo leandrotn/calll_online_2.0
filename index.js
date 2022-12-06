56fec7532f594ff70d865386031ec30d90c36962
leandrotn <74495390+leandrotn@users.noreply.github.com>
2022-12-06 18:10:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-//caso queira muda a bolinha de status: 
-//(status: '') 
-//mude os valores dos meios das aspas simples pelos status: dnd, idle, online, invisible)
-//Bom configs simples
-//Se você definir true para (setSelfDeaf) a conta será surda/false a conta continua com fone aberto.
-
-//Se você definir true para (setSelfMute) a conta será mutada/false a conta continuara desmutada.
-// caso queira definir atividade de jogos só colocar o nome em: { name: '' }.
-//Script desenvolvido by:LEANDROTN#0101 O BRABO DA 192
-
-
-// =============== Bot ===============
 const Discord = require('discord.js-self');
 
 
